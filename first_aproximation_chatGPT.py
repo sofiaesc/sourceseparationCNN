9f726cfdfc9ed0cdbd68bc8a534820aa864fc8d8
NPighin <npighin00@gmail.com>
2023-11-12 19:18:15 -0300

    coments

--- first_aproximation_chatGPT.py
+++ first_aproximation_chatGPT.py
@@ -4,7 +4,13 @@ def convolutional_autoencoder(input_shape):
     model = tf.keras.Sequential()
 
     # Encoding Stage
-    model.add(tf.keras.layers.Conv2D(30, kernel_size=(1, 30), strides=(1, 5), input_shape=input_shape, padding='same'))
+    model.add(tf.keras.layers.Conv2D(
+    30,                   # Número de filtros (neuronas) en la capa. En este caso, se utilizan 30 filtros.
+    kernel_size=(1, 30),  # Tamaño del kernel de convolución. En este caso, es una convolución 1x30.
+    strides=(1, 5),       # Longitud del paso durante la convolución. (1, 5) significa un paso de 1 en la dimensión vertical y 5 en la dimensión horizontal.
+    input_shape=input_shape,  # Forma de entrada esperada para esta capa. En este caso, se espera que la entrada tenga la forma especificada por la variable input_shape.
+    padding='same'        # Método de relleno. 'same' significa que se aplicará relleno para mantener el tamaño de la salida igual que el tamaño de la entrada, si es necesario.
+    ))
     model.add(tf.keras.layers.Conv2D(30, kernel_size=(2, 3 * input_shape[1]), strides=(1, 1), padding='valid'))
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(256, activation='relu'))
